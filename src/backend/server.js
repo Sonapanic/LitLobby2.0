c6e31eb3411f01f372e926f4e581d6fbb68c0993
Isaac Summers <monogator@gmail.com>
2023-10-24 14:42:17 -0600

    User registration form functional

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,190 +1,212 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
-import pg from 'pg'
-const { Pool } = pg
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import bcrypt, { hash } from "bcrypt";
+import pg from "pg";
+const { Pool } = pg;
 
+const app = express();
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+app.use(express.static("dist"));
 
-const app = express()
-dotenv.config()
-app.use(cors())
-app.use(express.json())
-app.use(express.static('dist'))
+const port = process.env.PORT;
 
-const port = process.env.PORT
-
-
-const pool = new Pool (
-    {connectionString: process.env.DATABASE_URL}
-)
-
-
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
 // GET requests
-app.get('/users', async (req, res) => {
-    try {
-        const users = await pool.query('SELECT * FROM users ORDER BY userId ASC')
-        res.status(200).json(users.rows)
-    } catch (err) {
-        console.error(err)
-        res.status(500).send('User request server error')
+app.get("/users", async (req, res) => {
+  try {
+    const users = await pool.query("SELECT * FROM users ORDER BY userId ASC");
+    res.status(200).json(users.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("User request server error");
+  }
+});
+
+app.get("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await pool.query("SELECT * FROM users WHERE userId = $1", [
+      id,
+    ]);
+    if (user.rowCount === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.status(200).json(user.rows[0]);
     }
-})
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("User request server error");
+  }
+});
 
-
-app.get('/users/:id', async (req, res) => {
-    const { id } = req.params
-    try {
-        const user = await pool.query('SELECT * FROM users WHERE userId = $1', [id])
-        if (user.rowCount === 0) {
-            res.status(404).send('User not found')
-        } else {
-            res.status(200).json(user.rows[0])
-        }
-    } catch (err) {
-        console.error(err)
-        res.status(500).send('User request server error')
+app.get("/books/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const books = await pool.query(
+      "SELECT * FROM books WHERE userId = $1 ORDER BY bookId ASC",
+      [id]
+    );
+    if (books.rowCount === 0) {
+      res
+        .status(404)
+        .send("Specified user either doesn't exist or has no books to view");
+    } else {
+      res.status(200).json(books.rows);
     }
-})
-
-
-app.get('/books/:id', async (req, res) => {
-    const { id } = req.params
-    try {
-        const books = await pool.query ('SELECT * FROM books WHERE userId = $1 ORDER BY bookId ASC', [id])
-        if (books.rowCount === 0) {
-            res.status(404).send('Specified user either doesn\'t exist or has no books to view')
-        } else {
-            res.status(200).json(books.rows)
-        }
-    } catch (err) {
-        console.error(err)
-        res.status(500).send('Book request server error')
-    }
-})
-
-
-
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Book request server error");
+  }
+});
 
 // POST requests
 
-app.post('/users', async (req, res) => {
-    const { username, hashed_password, email, first_name, last_name } = req.body
-    try {
-        const newUser = await pool.query('INSERT INTO users (username, hashed_password, email, first_name, last_name) VALUES ($1, $2, $3, $4, $5) RETURNING *', [username, hashed_password, email, first_name, last_name])
-        res.status(201).json(newUser.rows[0])
-    } catch (err) {
-        console.error(err)
-        res.status(500).send('User creation server error')
+// app.post('/users', async (req, res) => {
+//     const { username, hashed_password, email, first_name, last_name } = req.body
+//     try {
+//         const newUser = await pool.query('INSERT INTO users (username, hashed_password, email, first_name, last_name) VALUES ($1, $2, $3, $4, $5) RETURNING *', [username, hashed_password, email, first_name, last_name])
+//         res.status(201).json(newUser.rows[0])
+//     } catch (err) {
+//         console.error(err)
+//         res.status(500).send('User creation server error')
+//     }
+// })
+
+app.post("/books", async (req, res) => {
+  const { userId, title, author, description, genre, total_pages, pages_read } =
+    req.body;
+  try {
+    const newBook = await pool.query(
+      "INSERT INTO books (userId, title, author, description, genre, total_pages, pages_read) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+      [userId, title, author, description, genre, total_pages, pages_read]
+    );
+    if (newBook.rowCount === 0) {
+      res
+        .status(404)
+        .send("Specified user either doesn't exist or has no books to view");
+    } else {
+      res.status(200).json(newBook.rows[0]);
     }
-})
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Book creation server error");
+  }
+});
 
-
-app.post('/books', async (req, res) => {
-    const { userId, title, author, description, genre, total_pages, pages_read } = req.body
-    try {
-        const newBook = await pool.query('INSERT INTO books (userId, title, author, description, genre, total_pages, pages_read) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *', [userId, title, author, description, genre, total_pages, pages_read])
-        if (newBook.rowCount === 0) {
-            res.status(404).send('Specified user either doesn\'t exist or has no books to view')
-        } else {
-            res.status(200).json(newBook.rows[0])
-        }
-    } catch (err) {
-        console.error(err)
-        res.status(500).send('Book creation server error')
+app.post("/register", async (req, res) => {
+  const { username, password, email, first_name, last_name } = req.body;
+  try {
+    const comparison = await pool.query(
+      "SELECT EXISTS (SELECT 1 FROM users WHERE username = $1 OR email = $2)",
+      [username, email]
+    );
+    if (comparison.rows[0].exists === false) {
+      const hashed_password = await bcrypt.hash(password, 10);
+      const newUser = await pool.query(
+        "INSERT INTO users (username, hashed_password, email, first_name, last_name) VALUES ($1, $2, $3, $4, $5)",
+        [username, hashed_password, email, first_name, last_name]
+      );
+      res.status(201).json(newUser.rows[0]);
+    } else {
+      res.status(400).send(comparison.rows[0]);
     }
-})
-
-
-app.post('/login', async (req, res) => {
-    
-})
-
-
-
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("User registration error");
+  }
+});
 
 // DELETE requests
 
-app.delete('/users/:id', async (req, res) => {
-    const { id } = req.params
-    try {
-        const deletedUser = await pool.query('DELETE FROM users WHERE userId = $1 RETURNING *', [id])
-        if (deletedUser.rowCount === 0) {
-            res.status(404).send('User not found')
-        } else {
-            res.status(201).json(deletedUser.rows[0])
-        }
-    } catch (err) {
-        console.error(err)
-        res.status(500).send('User deletion server error')
+app.delete("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deletedUser = await pool.query(
+      "DELETE FROM users WHERE userId = $1 RETURNING *",
+      [id]
+    );
+    if (deletedUser.rowCount === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.status(201).json(deletedUser.rows[0]);
     }
-})
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("User deletion server error");
+  }
+});
 
-
-app.delete('/books/:userId/:bookId', async (req, res) => {
-    const { userId, bookId } = req.params
-    try {
-        const deletedBook = await pool.query('DELETE FROM books WHERE userId = $1 AND bookId = $2 RETURNING *', [userId, bookId])
-        if (deletedBook.rowCount === 0) {
-            res.status(404).send('Book not found')
-        } else {
-            res.status(201).json(deletedBook.rows[0])
-        }
-    } catch (err) {
-        console.error(err)
-        res.status(500).send('Book deletion server error')
+app.delete("/books/:userId/:bookId", async (req, res) => {
+  const { userId, bookId } = req.params;
+  try {
+    const deletedBook = await pool.query(
+      "DELETE FROM books WHERE userId = $1 AND bookId = $2 RETURNING *",
+      [userId, bookId]
+    );
+    if (deletedBook.rowCount === 0) {
+      res.status(404).send("Book not found");
+    } else {
+      res.status(201).json(deletedBook.rows[0]);
     }
-})
-
-
-
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Book deletion server error");
+  }
+});
 
 // UPDATE requests
-app.put('/users/:id', async (req, res) => {
-    const { id } = req.params
-    const { username, hashed_password, email, first_name, last_name } = req.body
-    try {
-        const revisedUser = await pool.query('UPDATE users set username = $1, hashed_password = $2, email = $3, first_name = $4, last_name = $5 WHERE userId = $6 RETURNING *', [username, hashed_password, email, first_name, last_name, id])
-        if (revisedUser.rowCount === 0) {
-            res.status(404).send('User not found')
-        } else {
-            res.status(201).json(revisedUser.rows[0])
-        }
-    } catch (err) {
-        console.error(err)
-        res.status(500).send('User info update server error')
+app.put("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  const { username, hashed_password, email, first_name, last_name } = req.body;
+  try {
+    const revisedUser = await pool.query(
+      "UPDATE users set username = $1, hashed_password = $2, email = $3, first_name = $4, last_name = $5 WHERE userId = $6 RETURNING *",
+      [username, hashed_password, email, first_name, last_name, id]
+    );
+    if (revisedUser.rowCount === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.status(201).json(revisedUser.rows[0]);
     }
-})
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("User info update server error");
+  }
+});
 
-
-app.put('/books/:userId/:bookId', async (req, res) => {
-    const { userId, bookId } = req.params
-    const { title, author, description, genre, total_pages, pages_read } = req.body
-    try {
-        const revisedBook = await pool.query('UPDATE books set title = $1, author = $2, description = $3, genre = $4, total_pages = $5, pages_read = $6 WHERE userId = $7 AND bookId = $8 RETURNING *', [title, author, description, genre, total_pages, pages_read, userId, bookId])
-        if (revisedBook.rowCount === 0) {
-            res.status(404).send('User not found')
-        } else {
-            res.status(201).json(revisedBook.rows[0])
-        }
-    } catch (err) {
-        console.error(err)
-        res.status(500).send('Book info update server error')
+app.put("/books/:userId/:bookId", async (req, res) => {
+  const { userId, bookId } = req.params;
+  const { title, author, description, genre, total_pages, pages_read } =
+    req.body;
+  try {
+    const revisedBook = await pool.query(
+      "UPDATE books set title = $1, author = $2, description = $3, genre = $4, total_pages = $5, pages_read = $6 WHERE userId = $7 AND bookId = $8 RETURNING *",
+      [
+        title,
+        author,
+        description,
+        genre,
+        total_pages,
+        pages_read,
+        userId,
+        bookId,
+      ]
+    );
+    if (revisedBook.rowCount === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.status(201).json(revisedBook.rows[0]);
     }
-})
-
-
-
-
-
-
-
-
-
-
-
-
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Book info update server error");
+  }
+});
 
 app.listen(port, () => {
-    console.log(`LitLobby up on ${port}`)
-})
+  console.log(`LitLobby up on ${port}`);
+});
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,31 +5,78 @@ const Login = () => {
   const { currentUser, setCurrentUser, newUser, setNewUser } =
     useContext(AuthContext);
 
-  const [userInfo, setUserInfo] = useState({ username: "", password: "" });
+  const renderUrl = "https://litlobby.onrender.com";
+  const localUrl = "http://localhost:10000";
 
-  const handleChange = (e) => {
-    const {name, value} = e.target
-    setUserInfo({...userInfo, [name]: value})
+  const [userInfo, setUserInfo] = useState({
+    username: "",
+    password: "",
+    email: "",
+    first_name: "",
+    last_name: "",
+  });
+
+  const [userCreated, setUserCreated] = useState({isNew: false, rejected: false});
+
+  const clearForm = () => {
+    setUserInfo({
+      username: "",
+      password: "",
+      email: "",
+      first_name: "",
+      last_name: "",
+    });
   };
 
-  const handleAccountSubmit = (e) => {
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserInfo({ ...userInfo, [name]: value });
+  };
+
+  const handleAccountSubmit = async (e) => {
+    // post request to make a new user
     e.preventDefault();
+
+    const options = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(userInfo),
+    };
+
+    try {
+      await fetch(`${localUrl}/register`, options);
+      clearForm();
+      setUserCreated({...userCreated, isNew: true, rejected: false});
+    } catch (err) {
+      console.error("Couldn't log in")
+      setUserCreated({...userCreated, isNew: false, rejected: true})
+    }
   };
 
   const handleLoginSubmit = (e) => {
+    // post request to authenticate returning user
     e.preventDefault();
-    setCurrentUser({
-      username: "test",
-      firstName: "isaac",
-      lastName: "summers",
-    });
+    // setCurrentUser({
+    //   username: "test",
+    //   firstName: "isaac",
+    //   lastName: "summers",
+    // });
 
-    // if !newUser, do login fetch request and setCurrentUser to the user's credentials
-
-    // else, submit a users post request and make a toast upon completion prompting a login
+    const options = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(userInfo),
+    };
   };
 
   const handleSignUp = () => {
+    clearForm();
     setNewUser(!newUser);
   };
 
@@ -39,26 +86,53 @@ const Login = () => {
       <form onSubmit={handleAccountSubmit} className="w-full max-w-xs">
         <div className="mb-4">
           <label className="block mb-2">Username</label>
-          <input type="text" className="border border-gray-300 p-1 w-full" />
+          <input
+            type="text"
+            className="border border-gray-300 p-1 w-full"
+            name="username"
+            value={userInfo.username}
+            onChange={handleChange}
+          />
         </div>
         <div className="mb-4">
           <label className="block mb-2">Password</label>
           <input
             type="password"
             className="border border-gray-300 p-1 w-full"
+            name="password"
+            value={userInfo.password}
+            onChange={handleChange}
           />
         </div>
         <div className="mb-4">
           <label className="block mb-2">Email</label>
-          <input type="email" className="border border-gray-300 p-1 w-full" />
+          <input
+            type="email"
+            className="border border-gray-300 p-1 w-full"
+            name="email"
+            value={userInfo.email}
+            onChange={handleChange}
+          />
         </div>
         <div className="mb-4">
           <label className="block mb-2">First Name</label>
-          <input type="text" className="border border-gray-300 p-1 w-full" />
+          <input
+            type="text"
+            className="border border-gray-300 p-1 w-full"
+            name="first_name"
+            value={userInfo.first_name}
+            onChange={handleChange}
+          />
         </div>
         <div className="mb-4">
           <label className="block mb-2">Last Name</label>
-          <input type="text" className="border border-gray-300 p-1 w-full" />
+          <input
+            type="text"
+            className="border border-gray-300 p-1 w-full"
+            name="last_name"
+            value={userInfo.last_name}
+            onChange={handleChange}
+          />
         </div>
         <input
           type="submit"
@@ -72,6 +146,16 @@ const Login = () => {
           Back to Login
         </button>
       </form>
+      {userCreated.isNew ? (
+        <span className="text-green-500">You've been successfully registered to LitLobby! Please log in to continue.</span>
+      ) : (
+        <span></span>
+      )}
+      {userCreated.rejected ? (
+        <span>Either the username or email you entered has already been registered. Please try a new one.</span>
+      ) : (
+        <span></span>
+      )}
     </div>
   ) : (
     <div className="flex flex-col items-center">
