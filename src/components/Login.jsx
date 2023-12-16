f4c182e3adb480b5168a5167ab5740087bd50c42
Isaac Summers <monogator@gmail.com>
2023-12-16 09:36:44 -0700

    Styling registry form. Removed green span, resizing form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,16 +1,22 @@
 import AuthContext from "../context/AuthContext";
 import { useContext, useState } from "react";
 
-
-
 const Login = () => {
-  const { currentUser, setCurrentUser, newUser, setNewUser, showToast, renderUrl } =
-    useContext(AuthContext);
+  const {
+    currentUser,
+    setCurrentUser,
+    newUser,
+    setNewUser,
+    showToast,
+    renderUrl,
+  } = useContext(AuthContext);
 
+  const [confirmPass, setConfirmPass] = useState("");
+  const changeConfirm = (e) => {
+    setConfirmPass(e.currentTarget.value);
+  };
 
-
-
-  const [userInfo, setUserInfo] = useState({ 
+  const [userInfo, setUserInfo] = useState({
     username: "",
     password: "",
     email: "",
@@ -23,9 +29,7 @@ const Login = () => {
     rejected: false,
   });
 
-  const isPasswordValid = userInfo.password.length >= 8
-
-
+  const isPasswordValid = userInfo.password.length >= 8;
 
   const clearForm = () => {
     setUserInfo({
@@ -58,15 +62,13 @@ const Login = () => {
     try {
       await fetch(`${renderUrl}/register`, options);
       clearForm();
-      handleSignUp()
+      handleSignUp();
       setUserCreated({ ...userCreated, isNew: true, rejected: false });
-      showToast('Account created! Please log in.')
+      showToast("Account created! Please log in.");
     } catch (err) {
       console.error("Couldn't sign up");
       setUserCreated({ ...userCreated, isNew: false, rejected: true });
     }
-
-
   };
 
   const handleSignUp = () => {
@@ -91,116 +93,106 @@ const Login = () => {
       const response = await fetch(`${renderUrl}/login`, options);
 
       if (response.ok) {
-        const user = await response.json()
-        const safeUser = Object.fromEntries(Object.entries(user).filter(([key]) => key !== 'hashed_password'))
-        localStorage.setItem('user', JSON.stringify(safeUser))
-        setCurrentUser(user)
-        showToast(`Welcome to LitLobby!`)
+        const user = await response.json();
+        const safeUser = Object.fromEntries(
+          Object.entries(user).filter(([key]) => key !== "hashed_password")
+        );
+        localStorage.setItem("user", JSON.stringify(safeUser));
+        setCurrentUser(user);
+        showToast(`Welcome to LitLobby!`);
       } else {
-        throw new Error('Login response not ok')
+        throw new Error("Login response not ok");
       }
-
     } catch (err) {
       console.error("Couldn't log in");
     }
   };
 
   return newUser ? (
-    <div className="flex flex-col items-center w-90">
+    <div className="flex flex-col items-center w-[25dvh]">
       <h2 className="mb-4 mt-20">Please enter the following information:</h2>
-      <form onSubmit={handleAccountSubmit} className="w-full max-w-xs">
-        <div className="mb-4">
-          <label className="block mb-2">Username</label>
-          <input
-            type="text"
-            className="border border-gray-300 p-1 w-full"
-            name="username"
-            value={userInfo.username}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block mb-2">Password</label>
-          <input
-            type="password"
-            className="border border-gray-300 p-1 w-full"
-            name="password"
-            value={userInfo.password}
-            onChange={handleChange}
-            required
-          />
-          {isPasswordValid ? <span></span> : <span>Passwords should contain at least 8 characters</span>}
-        </div>
-        <div className="mb-4">
-          <label className="block mb-2">Email</label>
-          <input
-            type="email"
-            className="border border-gray-300 p-1 w-full"
-            name="email"
-            value={userInfo.email}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block mb-2">First Name</label>
-          <input
-            type="text"
-            className="border border-gray-300 p-1 w-full"
-            name="first_name"
-            value={userInfo.first_name}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block mb-2">Last Name</label>
-          <input
-            type="text"
-            className="border border-gray-300 p-1 w-full"
-            name="last_name"
-            value={userInfo.last_name}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        
-        {isPasswordValid ? 
-          <input
-          type="submit"
-          className="bg-gray-200 p-2 cursor-pointer"
-          value="Submit"
-          onSubmit={handleAccountSubmit}
-        />
-        : 
-        <button className="bg-blue-200 p-2 cursor-pointer">
-          Submit
-        </button>
-      }
-        <button
-          onClick={handleSignUp}
-          className="bg-gray-200 p-2 cursor-pointer ml-24"
-        >
-          Back to Login
-        </button>
-      </form>
-      {userCreated.isNew ? (
-        <span className="text-green-500">
-          You've been successfully registered to Lit-Lobby! Please log in to
-          continue.
-        </span>
-      ) : (
-        <span></span>
-      )}
-      {userCreated.rejected ? (
-        <span>
-          Either the username or email you entered has already been registered.
-          Please try a new one.
-        </span>
-      ) : (
-        <span></span>
-      )}
+      <div className="w-full flex justify-center">
+        <form onSubmit={handleAccountSubmit} className="w-full flex flex-col ">
+          <div className="mb-4">
+            <label className="block mb-2">Username</label>
+            <input
+              type="text"
+              className="border border-gray-300 p-1 w-full"
+              name="username"
+              value={userInfo.username}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block mb-2">Password</label>
+            <input
+              type="password"
+              className="border border-gray-300 p-1 w-full"
+              name="password"
+              value={userInfo.password}
+              onChange={handleChange}
+              required
+            />
+            {isPasswordValid ? (
+              <span></span>
+            ) : (
+              <span className="w-1/2">Should contain at least 8 characters</span>
+            )}
+          </div>
+          <div className="mb-4">
+            <label className="block mb-2">Email</label>
+            <input
+              type="email"
+              className="border border-gray-300 p-1 w-full"
+              name="email"
+              value={userInfo.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block mb-2">First Name</label>
+            <input
+              type="text"
+              className="border border-gray-300 p-1 w-full"
+              name="first_name"
+              value={userInfo.first_name}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block mb-2">Last Name</label>
+            <input
+              type="text"
+              className="border border-gray-300 p-1 w-full"
+              name="last_name"
+              value={userInfo.last_name}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="w-full flex justify-between">
+          {isPasswordValid ? (
+            <input
+              type="submit"
+              className="bg-gray-200 p-2 cursor-pointer"
+              value="Submit"
+              onSubmit={handleAccountSubmit}
+            />
+          ) : (
+            <button className="bg-blue-200 p-2 cursor-pointer w-20">Submit</button>
+          )}
+          <button
+            onClick={handleSignUp}
+            className="bg-gray-200 p-2 cursor-pointer"
+          >
+            Back to Login
+          </button>
+          </div>
+        </form>
+      </div>
     </div>
   ) : (
     <div className="flex flex-col items-center mt-20">
