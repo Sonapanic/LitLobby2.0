9d2d90188bba2733edf8da4e0306caa5b546d370
Isaac Summers <monogator@gmail.com>
2024-03-28 14:09:44 -0600

    Fixed About componenet, added some text and styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,26 +1,28 @@
-
-
-
-
-
-
-
-
-
 const About = () => {
-
-
-    return (
-        <div className="h- w-full flex bg-softWhite justify-center">
-            Ayy
+  return (
+    <div className="w-full flex justify-center">
+      <div
+        className="h-1/2 w-1/2 p-5 mt-40 flex flex-wrap rounded-md bg-darkCream items-center justify-center"
+        style={{
+          background:
+            "linear-gradient(to bottom left, #E1D4C0, #cdbfa8, #E1D4C0)",
+        }}
+      >
+        <div className="p-2">
+          Thanks for visiting Lit-Lobby! The website is currently in a state of
+          change, so some features may not be working correctly.
         </div>
-    )
 
+        <div className="">
+          Books are now sorted into libraries, so until those components are
+          created and tested, there will be limited functionality for adding to
+          your lobby. This should be finished quite soon, thank you for being
+          patient!
+        </div>
+        <i className="text-lg">-Isaac</i>
+      </div>
+    </div>
+  );
+};
 
-}
-
-
-
-
-
-export default About
+export default About;
